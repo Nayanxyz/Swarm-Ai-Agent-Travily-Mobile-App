b858a7277146a3795d7d7b5fee007ff16e0c2bab
NayanPython <choudharyji527@gmail.com>
2026-05-18 11:32:00 +0530

    changed error messgae

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -178,7 +178,7 @@ export default function App() {
 
     } catch (error) {
       const errorMsg = { id: Date.now().toString(), text: "Sorry bhai, server se connection toot gaya! 🔌", sender: 'ai' };
-      setMessages((prevMessages) => [...prevMessages, errorMsg]);
+      setMessages((prevMessages) => [errorMsg, ...prevMessages]);
     } finally {
       setIsLoading(false);
     }
